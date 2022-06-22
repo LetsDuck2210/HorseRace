9e03fa73b28dd5940a064ce7916f71dad774c4c3
Joshi <joshua@lipps.de>
2022-06-22 14:44:40 +0200

    removed height-related speed changes

--- src/de/letsduck/horserace/listeners/HorseRideListener.java
+++ src/de/letsduck/horserace/listeners/HorseRideListener.java
@@ -1,12 +1,10 @@
 package de.letsduck.horserace.listeners;
 
 import org.bukkit.Bukkit;
-import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Horse;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.event.vehicle.VehicleExitEvent;
 
 import de.letsduck.horserace.main.Main;
@@ -26,43 +24,6 @@ public class HorseRideListener implements Listener {
 		
 		event.setCancelled(true);
 	}
-	
-	
-	@EventHandler
-	public void onHorseChangeY(PlayerMoveEvent event) {
-		if(!Main.isEnabled) return;
-		
-		if(event.getPlayer().getVehicle() == null) return;
-		if(!(event.getPlayer().getVehicle() instanceof Horse)) return;
-		
-		Horse horse = (Horse) event.getPlayer().getVehicle();
-		
-		if(!horse.getCustomName().equals(Main.HORSE_NAME)) return;
-
-		yChange(horse, event.getTo().getY());
-	}
-	@EventHandler
-	public void onHorseChangeYTeleport(PlayerTeleportEvent event) {
-		if(!Main.isEnabled) return;
-		
-		if(event.getPlayer().getVehicle() == null) return;
-		if(!(event.getPlayer().getVehicle() instanceof Horse)) return;
-		
-		Horse horse = (Horse) event.getPlayer().getVehicle();
-		
-		if(!horse.getCustomName().equals(Main.HORSE_NAME)) return;
-		
-		yChange(horse, event.getTo().getY());
-	}
-	private void yChange(Horse horse, double to) {
-		double yDis = to - yBase;
-		
-		yDis = Math.min(15, yDis);
-		yDis = Math.max(-6, yDis);
-		
-		horse.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(SPEED_BASE - yDis * Main.SPEED_CHANGE);
-	}
-	
 	
 	@EventHandler
 	public void onHorsePassFlag(PlayerMoveEvent event) {
